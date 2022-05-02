796b5537e8d909363e95fed6ca8eb3d07dd88689
SanjnaB <80415110+SanjnaB@users.noreply.github.com>
2022-05-02 18:08:33 -0400

    mychanges

--- models/Recipes.js
+++ models/Recipes.js
@@ -3,11 +3,10 @@ const mongoose = require( 'mongoose' );
 const Schema = mongoose.Schema;
 const ObjectId = Schema.Types.ObjectId;
 
-var movieSchema = Schema( {
+var recipeSchema = Schema( {
   userId: ObjectId,
   Title: String,
-  Director: String,
-  Year: Number
+  Cuisine: String,
 } );
 
-module.exports = mongoose.model( 'Recipe', movieSchema );
+module.exports = mongoose.model( 'Recipe', recipeSchema );
